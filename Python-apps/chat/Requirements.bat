e20e8a203f7690950c2714401d1f371aef023e0a
LockMan007 <LockMan007@users.noreply.github.com>
2025-08-12 03:00:59 -0400

    Update Requirements.bat

--- Python-apps/chat/Requirements.bat
+++ Python-apps/chat/Requirements.bat
@@ -6,5 +6,12 @@ python -m venv venv
 call .\venv\Scripts\activate.bat
 pip install pycryptodomex
 pip install cryptography
+pause
+echo I don't think you have to install crypto, i think this was a mistake. I have spent hours working on this, and finally got it doing the most basic thing possible, so it's time for a break and then i will come back and remove this if it is not used.
+echo.
+echo if you still want to install this anyway, then you can press any key to continue, otherwise you can leave this window open and test it and see if it says it is missing first.
+pause
+echo okay, installing "crypto"
 pip install crypto
+echo done.
 pause
