0f5f4cc71cfe0dd3de6a370d8b507efac53f11eb
LockMan007 <LockMan007@users.noreply.github.com>
2026-01-08 14:47:06 -0500

    Update LTX-2_16gbVRAM_100gb_SwapFile.bat

--- ComfyUI-misc/Gemma-LTX2/LTX-2_16gbVRAM_100gb_SwapFile.bat
+++ ComfyUI-misc/Gemma-LTX2/LTX-2_16gbVRAM_100gb_SwapFile.bat
@@ -1,4 +1,3 @@
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build  --use-sage-attention --reserve-vram 4
 echo If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest.
 pause
-.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --disable-cuda-malloc --disable-auto-launch
