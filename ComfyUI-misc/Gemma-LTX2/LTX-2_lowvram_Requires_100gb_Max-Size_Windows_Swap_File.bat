d1f2ae131d184f9d14094b54caf405cf6a854ee1
LockMan007 <LockMan007@users.noreply.github.com>
2026-01-09 17:55:15 -0500

    removed WAS node requirement

---
+++ ComfyUI-misc/Gemma-LTX2/LTX-2_lowvram_Requires_100gb_Max-Size_Windows_Swap_File.bat
@@ -0,0 +1,6 @@
+.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --disable-auto-launch --use-sage-attention --reserve-vram 4
+
+ECHO If you see this and ComfyUI did not start try updating your Nvidia Drivers to the latest.
+ECHO This is just a generic message after crashing and does not actually indicate any problem with Nvidia Drivers.
+
+PAUSE
